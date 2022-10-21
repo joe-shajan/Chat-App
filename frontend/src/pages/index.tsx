dea5423339d052d0c921ad28a1f0d2f274af7f2d
joeshajan <joeshajan1551@gmail.com>
2022-10-21 22:57:42 +0530

    first commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data } = useSession();
+  console.log(data);
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>sign out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>sign in</button>
+      )}
+
+      {data?.user?.name}
+    </div>
+  );
+};
+
+export default Home;
