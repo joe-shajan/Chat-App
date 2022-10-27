6c8dc725d01926b60ac32a72a9d578f2cf09b7d3
joeshajan <joeshajan1551@gmail.com>
2022-10-27 21:18:11 +0530

    added apollo client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
 import theme from "../chakra/theme";
-
+import { client } from "../graphql/apollo-client";
 
 function MyApp({
   Component,
@@ -12,11 +13,13 @@ function MyApp({
   session: Session;
 }>) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={pageProps.session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
